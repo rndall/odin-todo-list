a934a18fd10a77891a0c451565069b71e4f4ebba
Ran <96964503+rndall@users.noreply.github.com>
2025-03-01 18:40:32 +0800

    Add status toggling and changing priority of todo

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -1,3 +1,24 @@
 export default function createTodo(title, description, dueDate, priority) {
-  return { id: Date.now(), title, description, dueDate, priority }
+  let currPriority = priority
+  const getPriority = () => currPriority
+  const setPriority = (newPriority) => {
+    currPriority = newPriority
+  }
+
+  let status = false
+  const getStatus = () => status
+  const toggleStatus = () => {
+    status = !status
+  }
+
+  return {
+    id: Date.now(),
+    title,
+    description,
+    dueDate,
+    getStatus,
+    toggleStatus,
+    getPriority,
+    setPriority,
+  }
 }
