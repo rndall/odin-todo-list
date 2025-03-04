9048d60f67ac7a0eabd73cb0332b3c2801224fb0
Ran <96964503+rndall@users.noreply.github.com>
2025-03-04 15:15:33 +0800

    Add new todo adding functionality

--- src/modules/modals.js
+++ src/modules/modals.js
@@ -1,4 +1,4 @@
-import { addProject } from "./todo-list"
+import { addProject, getCurrentProject } from "./todo-list"
 import { updateProjectsNavList } from "./sidebar"
 import { updateMainPage } from "./main-page"
 
@@ -26,4 +26,39 @@ newProjectForm.addEventListener("submit", (e) => {
   updateMainPage(project.id)
 })
 
-export { showNewProjectModal }
+const newTodoDialog = document.querySelector("#new-todo")
+const closeNewTodoBtn = document.querySelector("#todo-close")
+const newTodoForm = document.querySelector("#todo-form")
+
+const todoTitle = document.querySelector("#title")
+const todoDesc = document.querySelector("#description")
+const todoDueDate = document.querySelector("#due_date")
+const todoPriority = document.querySelector("#priority")
+
+function showNewTodoModal() {
+  newTodoDialog.showModal()
+}
+
+closeNewTodoBtn.addEventListener("click", () => newTodoDialog.close())
+
+newTodoForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+
+  const project = getCurrentProject()
+  project.addTodo(
+    todoTitle.value.trim(),
+    todoDesc.value.trim(),
+    todoDueDate.value,
+    todoPriority.value
+  )
+
+  todoTitle.value = ""
+  todoDesc.value = ""
+  todoDueDate.value = ""
+  todoPriority.selectedIndex = 0
+
+  updateMainPage(project.id)
+  newTodoDialog.close()
+})
+
+export { showNewProjectModal, showNewTodoModal }
